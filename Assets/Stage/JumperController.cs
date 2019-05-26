97c72ae8fda4207d998d6401461ef6d75b478eb7
tori-takashi <aporia2718@outlook,com>
2019-05-26 17:28:47 +0800

    MeasureDistance functions has moved to JumperController

--- Assets/Stage/JumperController.cs
+++ Assets/Stage/JumperController.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class JumperController : MonoBehaviour {
     int clickCounter = 0;
     bool isJumperCollided = false;
+
+    float distance;
+
+    GameObject jumper;
+    GameObject measureStage;
     Vector3 addedForce;
     Rigidbody rbJumper;
     // Start is called before the first frame update
     void Start() {
         clickCounter = 0;
+
+        jumper = GameObject.Find("Jumper");
+        measureStage = GameObject.Find("MeasureStage");
 
         rbJumper = GetComponent<Rigidbody>();
         addedForce = new Vector3((float)clickCounter,0,0);
@@ -23,6 +31,11 @@ public class JumperController : MonoBehaviour {
     }
 
     void OnCollisionEnter(Collision collision) {
+       if(collision.gameObject.name == "MeasureStage") {
+            distance = Vector3.Distance(measureStage.transform.position, jumper.transform.position);
+            Debug.Log(distance);
+        }
+
         if (collision.gameObject.name == "Jumper") {
             isJumperCollided = true;
         }
