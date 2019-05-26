85124533d9eb3058c49856ecb82705bbe7d6d8a0
tori-takashi <aporia2718@outlook,com>
2019-05-26 17:11:59 +0800

    Enable to measure the distance of jumping

---
+++ Assets/Stage/JumperController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumperController : MonoBehaviour {
+    int clickCounter = 0;
+    bool isJumperCollided = false;
+    Vector3 addedForce;
+    Rigidbody rbJumper;
+    // Start is called before the first frame update
+    void Start() {
+        clickCounter = 0;
+
+        rbJumper = GetComponent<Rigidbody>();
+        addedForce = new Vector3((float)clickCounter,0,0);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (!isJumperCollided) {
+            AccelerateJumper();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.name == "Jumper") {
+            isJumperCollided = true;
+        }
+    }
+
+    void AccelerateJumper(){
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return)) {
+            clickCounter++;
+            addedForce.x = (float)clickCounter;
+        }
+        rbJumper.AddForce(addedForce, ForceMode.Acceleration);
+    }
+}
