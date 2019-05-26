d28a3c710e95ba2d113489690633b5dd5755c3fa
tori-takashi <aporia2718@outlook,com>
2019-05-26 19:03:09 +0800

    displaying the distance

--- Assets/Stage/JumperController.cs
+++ Assets/Stage/JumperController.cs
@@ -12,14 +12,16 @@ public class JumperController : MonoBehaviour {
     GameObject measureStage;
     Vector3 addedForce;
     Rigidbody rbJumper;
+
     // Start is called before the first frame update
     void Start() {
+        rbJumper = GetComponent<Rigidbody>();
+
         clickCounter = 0;
 
         jumper = GameObject.Find("Jumper");
         measureStage = GameObject.Find("MeasureStage");
 
-        rbJumper = GetComponent<Rigidbody>();
         addedForce = new Vector3((float)clickCounter,0,0);
     }
 
@@ -33,7 +35,6 @@ public class JumperController : MonoBehaviour {
     void OnCollisionEnter(Collision collision) {
        if(collision.gameObject.name == "MeasureStage") {
             distance = Vector3.Distance(measureStage.transform.position, jumper.transform.position);
-            Debug.Log(distance);
         }
 
         if (collision.gameObject.name == "Jumper") {
@@ -41,7 +42,11 @@ public class JumperController : MonoBehaviour {
         }
     }
 
-    void AccelerateJumper(){
+    public float getDistance() {
+        return distance;
+    }
+
+    void AccelerateJumper() {
         if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return)) {
             clickCounter++;
             addedForce.x = (float)clickCounter;
